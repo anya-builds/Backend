854b96da38865528f1b06b63ebea4b6612dfad42
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-07-14 06:16:04 +0530

    process object

---
+++ src/01-process-object.ts
@@ -0,0 +1,19 @@
+// env varibles
+// commaand line arguments
+// exit code
+// process lifecycle events
+
+// read backend port from env files
+// read secrets - db urls, api keys, password, google search auth secret
+// read cli arguments in scripts
+
+//process.env
+
+import process from "node:process"
+
+const nodeEnv = process.env.NODE_ENV ?? "development"
+
+
+
+
+
