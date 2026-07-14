b77255cdf4ed0d6924d4502d0be1ac49e0e9c79e
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-07-14 06:48:12 +0530

    env modules

--- src/01-process-object.ts
+++ src/01-process-object.ts
@@ -17,9 +17,40 @@
 // const port = Number(process.env.PORT?? 3000)
 
 
+// process.argv ->
+// [
+//     "/path/to/node",
+//     "src/01-process-object.ts",
+//     "start"
+// ]
+// process.argv[2]
 
+const command = process.argv[2] ?? "start";
+// fail flag
+// crash flag
 
+const shouldFail =  process.argv.includes("--fail")
+const shouldCrash = process.argv.includes("--crash")
 
+// do not start async here
+// node is already shutting down
+// final log, final cleanup
 
+process.on("exit",(code)=>{
+    console.log(`Process finished with exit code ${code}`)
+});
+
+function runApp():void{
+    console.log({
+        command,
+    });
+    if(shouldFail){
+        console.log("Manual failure triggered with --fail flag");
+    }   
+    if(shouldCrash){
+        console.log("Manual crash triggered with -- crash flag")
+    }
+}
+runApp();
 
 
