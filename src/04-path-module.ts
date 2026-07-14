34c6c5f31a203e3bc1242164c89a64c2d2b27d32
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-07-15 05:08:00 +0530

    path module

---
+++ src/04-path-module.ts
@@ -0,0 +1,28 @@
+// build and read file paths
+
+import path from "node:path"
+
+// const filePath = projectRoot + "/uploads" + filename
+
+// path.join : uses the correct separator for the current os
+// /users/aankita/project/fileURLToPath.text
+// c:\users\ankita\project\fileURLToPath.txt
+
+// process.cwd: the folder from where node js process was started
+
+const projectRoot = process.cwd()
+console.log(projectRoot)
+
+// /uploads/users/42/profile.photo.png
+const userId = "42"
+const originalName = "profile.photo.png"
+
+// imp -> path.jin -> creates a path string
+// it will not crete the folder
+// it does not check whether the file  exists or nott
+
+const uploadFilePath = path.join(
+    projectRoot, "uploads","users",userId, originalName
+)
+
+console.log(uploadFilePath)
