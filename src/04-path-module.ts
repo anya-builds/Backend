9f5c0c8bfc25aaef43c6bd579224a85de87de4b2
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-07-17 04:04:12 +0530

    object-module

--- src/04-path-module.ts
+++ src/04-path-module.ts
@@ -1,34 +1,35 @@
-// // build and read file paths
+// build and read file paths
 
-// import path from "node:path"
+import path from "node:path"
 
-// // const filePath = projectRoot + "/uploads" + filename
+// const filePath = projectRoot + "/uploads" + filename
 
-// // path.join : uses the correct separator for the current os
-// // /users/aankita/project/fileURLToPath.text
-// // c:\users\ankita\project\fileURLToPath.txt
+// path.join : uses the correct separator for the current os
+// /users/aankita/project/fileURLToPath.text
+// c:\users\ankita\project\fileURLToPath.txt
 
-// // process.cwd: the folder from where node js process was started
+// process.cwd: the folder from where node js process was started
 
-// const projectRoot = process.cwd()
-// console.log(projectRoot)
+const projectRoot = process.cwd()
+console.log(projectRoot)
 
-// // /uploads/users/42/profile.photo.png
-// const userId = "42"
-// const originalName = "profile.photo.png"
+// /uploads/users/42/profile.photo.png
+const userId = "42"
+const originalName = "profile.photo.png"
 
-// // imp -> path.jin -> creates a path string
-// // it will not crete the folder
-// // it does not check whether the file  exists or nott
+// imp -> path.jin -> creates a path string
+// it will not crete the folder
+// it does not check whether the file  exists or nott
 
-// const uploadFilePath = path.join(
-//     projectRoot, "uploads","users",userId, originalName
-// )
+const uploadFilePath = path.join(
+    projectRoot, "uploads","users",userId, originalName
+)
 
-// console.log(uploadFilePath);
+console.log(uploadFilePath);
 
-// // final part of a path
-// const fileName = path.basename(uploadFilePath)
-// const fileExt = path.extname(uploadFilePath);
+// final part of a path
+const fileName = path.basename(uploadFilePath)
+const fileExt = path.extname(uploadFilePath);
+const parentFolder = path.dirname(uploadFilePath);
 
-// console.log(fileName, fileExt)
+console.log(parentFolder)
