f1a710226642cdc047bf95320c6fb7463e78fc14
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-07-18 19:00:29 +0530

    callbaack

--- src/06-callback-promises-async-await-module.ts
+++ src/06-callback-promises-async-await-module.ts
@@ -1,4 +1,11 @@
-type User=[{
+import { error } from "node:console";
+
+type User={
+    id: number,
+    name: string,
+    role: "user" | "super-admin";
+}
+const users: User[]=[{
     id: 1,
     name: "Ankita",
     role:'super-admin'
@@ -20,4 +27,22 @@ type User=[{
 function findUserWithCallbck(
     userId: number,
     callbaack: (error: Error | null, user?: User)=> void
-)
+): void {
+    setTimeout(()=>{
+        // u are actual aapi cll
+        const user = users.find(currentUser=> currentUser.id === userId)
+        if(!user){
+            callbaack(new Error(`user with id ${userId} was not found`))
+            return;
+        }
+        callbaack(null, user)
+    },500)
+}
+
+findUserWithCallbck(3,(error, user)=>{
+    if(error){
+        console.log('callback error', error.message);
+        return;
+    }
+    console.log("callback result", user?.id, user?.name, user?.role);
+})
