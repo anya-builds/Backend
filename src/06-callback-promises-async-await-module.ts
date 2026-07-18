5cd0515dd6d2f7c4188c0e73440b7f5aa87e943c
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-07-18 18:44:10 +0530

    callbaack

--- src/06-callback-promises-async-await-module.ts
+++ src/06-callback-promises-async-await-module.ts
@@ -11,4 +11,13 @@ type User=[{
     id:3,
     name:'Rahul',
     role:'user'
-}]
+}];
+
+
+// callback is function that is paassed through another function as  parameter
+// callback(error, result)--> *** imp concepts -? claassic node js pattern
+
+function findUserWithCallbck(
+    userId: number,
+    callbaack: (error: Error | null, user?: User)=> void
+)
