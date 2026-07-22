91db987bf7403c8ecf7b097e75bee251e00aa64d
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-07-23 05:09:36 +0530

    buffer

--- src/07-fs-module.ts
+++ src/07-fs-module.ts
@@ -1,93 +1,126 @@
- import path from "node:path"
- import fs from 'node:fs'
-import { resolve } from "node:dns"
-const DEMO_FOLDER_PATH=  path.join(process.cwd(),'file-system','fs-demo')
-const SYNC_FILE_PATH = path.join(DEMO_FOLDER_PATH,'sync-note.txt')
-const CALLBACK_FILE_PATH= path.join(DEMO_FOLDER_PATH,"callback-note.txt")
-const PROMISE_FILE_PATH=path.join(DEMO_FOLDER_PATH,"promise-note.txt")
+import path from "node:path";
+import fs from "node:fs";
+import fsPromises from 'node:fs/promises'
+const DEMO_FOLDER_PATH = path.join(process.cwd(), "file-system", "fs-demo");
+const SYNC_FILE_PATH = path.join(DEMO_FOLDER_PATH, "sync-note.txt");
+const CALLBACK_FILE_PATH = path.join(DEMO_FOLDER_PATH, "callback-note.txt");
+const PROMISE_FILE_PATH = path.join(DEMO_FOLDER_PATH, "promise-note.txt");
 
-type fileResult={
-    style: string,
-    fileName: string,
+type FileResult = {
+    style: string;
+    fileName: string;
     content: string;
-    sizeBytes: number
-}
-// fs- file system
-// create folder
-// write files
-// read files
-// check filesinformation
-// delete files
+    sizeBytes: number;
+};
 
-// sync apis: fs.readFileSync
-// callback api
-//  promise api
-
-// small startup scripts
-// build scripts
-// local demos
-// not good or even bad practices
-// http req hndlers
-// high traffic apis
-// background apis
-
-function ensureDemoFolderExists(): void{
-    if(!fs.existsSync(DEMO_FOLDER_PATH)){
-        fs.mkdirSync(DEMO_FOLDER_PATH,{recursive: true})
+function ensureDemoFolderExists(): void {
+    if (!fs.existsSync(DEMO_FOLDER_PATH)) {
+        fs.mkdirSync(DEMO_FOLDER_PATH, { recursive: true });
     }
 }
-function runSyncExample():  fileResult{
-    // wriite content to a file
-    fs.writeFileSync(SYNC_FILE_PATH,"created using sync fs",'utf-8')
 
-    fs.appendFileSync(SYNC_FILE_PATH,"Appended using sync fs ",'utf-8')
+function runSyncExample(): FileResult {
+    fs.writeFileSync(SYNC_FILE_PATH, "created using sync fs", "utf-8");
+    fs.appendFileSync(SYNC_FILE_PATH, " Appended using sync fs", "utf-8");
 
-    const content=fs.readFileSync(SYNC_FILE_PATH,'utf-8')
+    const content = fs.readFileSync(SYNC_FILE_PATH, "utf-8");
     const stats = fs.statSync(SYNC_FILE_PATH);
 
     return {
         style: "sync",
-        content,
         fileName: path.basename(SYNC_FILE_PATH),
+        content,
         sizeBytes: stats.size,
-    }
-
+    };
 }
 
-async function main(): Promise<void>{
-    try{
-        ensureDemoFolderExists()
-        const syncResult = runSyncExample();
-        console.log(syncResult)
-    } catch (error) {
-    const message = error instanceof Error ? error.message : "Unknown Error";
-    console.error("File system error:", message);
-}
-} 
-// main()
-
-function runCallbackExample(): Promise<fileResult>{
-    return new Promise((resolve, reject)=>{
+function runCallbackExample(): Promise<FileResult> {
+    return new Promise((resolve, reject) => {
         fs.writeFile(
             CALLBACK_FILE_PATH,
             "created using callback fs",
             "utf-8",
-            (writeError)=>{
-                if(writeError){
-                    reject(writeError)
-                    return
+            (writeError) => {
+                if (writeError) {
+                    reject(writeError);
+                    return;
                 }
+
                 fs.appendFile(
                     CALLBACK_FILE_PATH,
-                    "Append using callback fs ",
+                    " Appended using callback fs",
                     "utf-8",
-                    (appendError)=> {
-                        reject(appendError)
-                        return
-                    }
+                    (appendError) => {
+                        if (appendError) {
+                            reject(appendError);
+                            return;
+                        }
 
-                )
+                        fs.readFile(
+                            CALLBACK_FILE_PATH,
+                            "utf-8",
+                            (readError, content) => {
+                                if (readError) {
+                                    reject(readError);
+                                    return;
+                                }
+
+                                fs.stat(
+                                    CALLBACK_FILE_PATH,
+                                    (statError, stats) => {
+                                        if (statError) {
+                                            reject(statError);
+                                            return;
+                                        }
+
+                                        resolve({
+                                            style: "callback",
+                                            fileName: path.basename(CALLBACK_FILE_PATH),
+                                            content,
+                                            sizeBytes: stats.size,
+                                        });
+                                    }
+                                );
+                            }
+                        );
+                    }
+                );
             }
-        )
-    })
+        );
+    });
 }
+
+
+// promises apis
+async function runPromiseExample(): Promise<FileResult>{
+    await fsPromises.writeFile(
+        PROMISE_FILE_PATH,
+        "Created using promise apis",
+        "utf-8"
+    )
+    await fsPromises.appendFile(
+        PROMISE_FILE_PATH,
+        " appended using promise apis",
+        "utf-8"
+    )
+    const content = await fsPromises.readFile(PROMISE_FILE_PATH,'utf-8')
+
+}
+async function main(): Promise<void> {
+    try {
+        ensureDemoFolderExists();
+
+        const syncResult = runSyncExample();
+        const callbackResult = await runCallbackExample();
+
+        console.log(syncResult);
+        console.log(callbackResult);
+    } catch (error) {
+        const message =
+            error instanceof Error ? error.message : "Unknown Error";
+
+        console.error("File system error:", message);
+    }
+}
+
+main();
---
+++ src/08-buffers.ts
@@ -0,0 +1,13 @@
+// buffers - raw binary data
+// binary data means - when u have ur data shared in bytes
+
+// reading files
+// receiveing http req bodies
+// working with streams
+// handling images, pdf files, vides
+// encrypt and hashing
+
+// string - human readable text
+// buffer - raw bytes
+
+const textBuffer = Buffer.from("Node")
