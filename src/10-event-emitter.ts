2768764f45a122d917399d5ca284a01ffa3b9edb
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-07-24 04:10:32 +0530

    emit event

---
+++ src/10-event-emitter.ts
@@ -0,0 +1,41 @@
+// user registered 
+// send a welcome email
+// write a log
+// notify some other seervice
+
+import { EventEmitter } from "node:events";
+
+// emitt one event -> listener listen to this event, do something
+
+// .on() -> register one lsitener
+// .once() ->  register oone listener that runs only one time
+// .emit() -> triggers an event and sends to the listeners
+
+const appEvents = new EventEmitter()
+
+type UserRegisterPayload = {
+    id: number;
+    email : string
+}
+
+appEvents.on("user:registered",(user: UserRegisterPayload)=>{
+    console.log(`email listener: welcome email sent to this user ${user.email}`)
+})
+
+appEvents.on("user:registered", (user: UserRegisterPayload)=>{
+    console.log(`log listener: user ${user.id} and email is ${user.email}`)
+})
+
+function regsiterUser(): void{
+    const user = {
+        id: 1,
+        email: 'ankitaarya@gmail.com'
+    }
+    console.log("user saved");
+
+    appEvents.emit("user:registered", user)
+
+    console.log("register user: event listeners completed.")
+}
+
+regsiterUser()
