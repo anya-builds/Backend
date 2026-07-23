30f6a410148df3084bab7eedf22e011ac7da6c96
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-07-24 03:55:18 +0530

    buffer

--- src/08-buffers.ts
+++ src/08-buffers.ts
@@ -21,3 +21,22 @@ console.log(textBuffer.toString('utf-8'))
 
 const engBuffer = Buffer.from("hello");
 console.log(engBuffer.length)
+
+//.alloc
+const fixedBuffer = Buffer.alloc(5);
+console.log("empty fixed buffer", fixedBuffer)
+
+fixedBuffer.write("API")
+
+console.log("fixed buffer after writee",fixedBuffer)
+console.log("fixed buffer as text", fixedBuffer.toString("utf-8"))
+
+// chunks
+const chunks = [
+    Buffer.from("Hello "),
+    Buffer.from("Node "),
+    Buffer.from("JS ")
+]
+
+const combinedBuffer = Buffer.concat(chunks);
+console.log(combinedBuffer, combinedBuffer.toString("utf-8"))
